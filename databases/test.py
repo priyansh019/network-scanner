acfc619a0e93b7ce6e876ddf2a6f2fdc7f2cda47
Priyansh Singh <priyansh.singh019@gmail.com>
2026-06-07 22:23:26 +0530

    streak 6

--- databases/test.py
+++ databases/test.py
@@ -3,11 +3,3 @@ import os
 with open("vulnerabilities.json") as file:
     data = json.load(file)
 print(data["Apache"])
-print("Hello World0")
-print("Hello World0")
-print("Hello World0")
-print("Hello World0")
-print("Hello World0")
-print("Hello World0")
-print("Hello World0")
-print("Hello World0")
