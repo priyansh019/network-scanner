73d833ded2b77561f1bcdf11204207618fb27dd5
Preet-kasana205git config --global user.email kasanap553@gmail.com <kasanap553@gmail.com>
2026-06-27 09:28:07 +0530

    add new url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://network-scanner-1-p3wn.onrender.com/api/v1'
+
+const BASE_URL = import.meta.env.VITE_API_URL + '/api/v1'
 
 const api = axios.create({ baseURL: BASE_URL })
 
